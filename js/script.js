86911414ef2d97532994957f471faa7765a3ad5b
Your Name <you@example.com>
2019-12-05 18:56:21 +0100

    Lesson 13

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+let result = 4 > 1;
+console.log("TCL: result", result)
+
+if (100 - 10 > 80) {
+  alert("Correct");
+} else {
+  alert("Not correct");
+}
+
 if (result) {
   document.getElementById("result").textContent = result;
 }
